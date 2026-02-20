76ce5b37a9baaa8029c7043cc9aaae8ad2e3d83c
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2026-02-19 22:52:05 -0500

    Create an example plugin

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Remember about Wordpress Development Basics
 
 Course: https://learn.wordpress.org/
+Plugin Best Practices: https://developer.wordpress.org/plugins/plugin-basics/best-practices/
---
+++ wp-content/plugins/example-plugin/example-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Example Plugin
+ * Description: This is an example plugin for the WordPress developer pathway.
+ * Version: 1.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
