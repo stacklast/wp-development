86457ce95ce12e478bd5b56492f8a122632f9339
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2026-02-19 22:28:02 -0500

    Add filter get_the_excerpt with params and order

--- wp-content/themes/twentytwentyfive/functions.php
+++ wp-content/themes/twentytwentyfive/functions.php
@@ -1,5 +1,5 @@
 <?php
-add_action( 'after_setup_theme', 'wp_learn_setup_theme');
+add_action( 'after_setup_theme', 'wp_learn_setup_theme', 11);
 
 function wp_learn_setup_theme() {
     add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
@@ -11,4 +11,12 @@ function wp_learn_amend_content( $content ) {
     $content            = $content . $additional_content;
 
     return $content;
+}
+
+add_filter( 'get_the_excerpt', 'wp_learn_amend_the_excerpt', 12, 2 );
+function wp_learn_amend_the_excerpt( $post_excerpt, $post ) {
+    $additional_content = '<p>'. $post->post_title . ' Verified by Search Engine</p>';
+    $post_excerpt       = $post_excerpt . $additional_content;
+
+    return $post_excerpt;
 }
